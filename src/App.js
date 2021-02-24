ae970fbbcdb79b0e1ba6053a79c61ec8798af68f
calaunte <calaunte@protonmail.com>
2021-02-23 21:40:47 -0500

    updated links

--- src/App.js
+++ src/App.js
@@ -46,13 +46,13 @@ function App() {
       <div>
       <div style={{display: `flex`, flexDirection: `column`, alignItems: `center`}}>
       <img src={logo} alt="zector logo" width="300" height="60" style={{marginBottom: `75px`, textAlign: `center`}}/>
+      <Link className="button" href="https://form.jotform.com/202067012412135">Ready to build an app? Get in touch</Link>
       <Link className="button" href="https://www.zector.io/about">Learn more about Zector</Link>
       <Link className="button" href="https://www.zector.io/services">Zector's Services</Link>
       <Link className="button" href="https://www.zector.io/">Zector's Process</Link>
       <Link className="button" href="https://www.zector.io/blog">Check out Zector's latest blog posts</Link>
       <Link className="button" href="https://offers.zector.io/build-a-loyal-fanbase-with-zectors-secret-tool-for-indie-entertainers">Download Our Free Guide<br/> Build a Loyal Fanbase With<br/> Zector's Secret Tool for Indie Entertainers</Link>
       <Link className="button" href="https://creative-producer-8722.ck.page/e8ac3e7b71">Interested in Software Engineering?<br/> Join our email list!</Link>
-      <Link className="button" href="https://form.jotform.com/202067012412135">Ready to build an app? Get in touch</Link>
       </div>
       </div>
     </Section>
